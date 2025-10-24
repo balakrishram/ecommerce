0b81836269e83161c3eee92553a93f55dd2e8de8
BALAKRISHNAN <balakrish007007@gmail.com>
2025-10-24 18:33:11 +0530

    Create multiple pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router'
 import { HomePage } from './pages/HomePage'
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
 
+function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/checkout" element={<div>Testing</div>}></Route>
+    </Routes>
   )
 }
 
