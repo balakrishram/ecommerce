10c6c054d36f0e222c34247f4b22e65d7c14b36a
BALAKRISHNAN <balakrish007007@gmail.com>
2025-10-24 19:14:14 +0530

    Move OrdersPage & TrackingPage into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckOutPage } from './pages/CheckOutPage'
-
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
-
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/checkout" element={<CheckOutPage/>}></Route>
+      <Route path="/orders" element={<OrdersPage/>}></Route>
+      <Route path="/tracking" element={<TrackingPage/>}></Route>
     </Routes>
   ) 
 }
