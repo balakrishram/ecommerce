29775cb66a8ababb0c1e3fff87aefff3c966df61
BALAKRISHNAN <balakrish007007@gmail.com>
2025-10-27 09:15:33 +0530

    Generate the HTML for the delivery options

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   useEffect(() => {
     axios('api/cart-items?expand=product')
       .then((response) => {
-        console.log(response.data);
         setCart(response.data);
       });
   }, []);
